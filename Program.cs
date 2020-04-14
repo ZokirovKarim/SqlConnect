35c0f441b3f4f0e3a91490d9e1c57931d48d3d36
Karim <karim.zokirov@mail.ru>
2020-04-14 19:27:52 +0500

    SQL Connect

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using system.Data.sqlClient;
+
+namespace sqlConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string conString=@"Data sourse=localhost; Initial catalog=schoolAlif; Integrated security=true"
+            SqlConnection con=new sql(conString);
+        }
+        con.Open();
+        if(con.State==SqlConnection.Open)
+        {
+            
+            
+        }
+    }
+}
